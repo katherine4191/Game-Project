268f1976b6aecd6e0078a14eb9b329e22fba5b99
Soyoung <ma2214191@gmail.com>
2021-02-28 00:45:22 +0900

    "Object"

---
+++ maple_story/maple_story/Obj.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Obj.h"
+
+CObj::CObj()
+{
+}
+
+//CObj::CObj(wstring _strObjTag, tPoint _ptPos)
+////: m_ptPos(tPoint(WINSIZE_X / 2, WINSIZE_Y / 2))
+//{
+//	m_strObjTag = _strObjTag;
+//	m_ptPos = _ptPos;
+//}
+
+CObj::~CObj()
+{
+}
---
+++ maple_story/maple_story/Obj.h
@@ -0,0 +1,28 @@
+#pragma once
+class CObj
+{
+protected:
+	wstring m_strObjTag;
+	//tPoint	m_ptPos;
+
+public:
+	virtual void init() {};
+	virtual int update() = 0;
+	virtual void render(HDC _dc) = 0;
+
+	//void SetPos(tPoint _pt) { m_ptPos = _pt; }
+	//tPoint GetPos() { return m_ptPos; }
+
+	void SetTag(const wstring& _strTag) { m_strObjTag = _strTag; }
+	const wstring& GetTag() { return m_strObjTag; }
+
+	//CAnimator* GetAnimator();
+
+	//virtual bool IsCollision() { return false; }
+
+public:
+	CObj();
+	//CObj(wstring _strObjTag, tPoint _ptPos);
+	virtual ~CObj();
+};
+
